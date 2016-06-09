95c0c4d18c2896ed403c83a3b079439da11b91fa
Mark Cooper <markchristophercooper@gmail.com>
2016-06-09 09:19:08 -0700

    Initial set of registered procedures for pp

---
+++ config/initializers/converters/pastperfect/_config.rb
@@ -0,0 +1,14 @@
+module CollectionSpace
+  module Converter
+    module PastPerfect
+
+      def self.registered_procedures
+        [
+          "Acquisition",
+          "CollectionObject",
+        ]
+      end
+
+    end
+  end
+end
