ba5fb8c57f7028bc1fbfb704aa0f999d8e37bb21
Mark Cooper <markchristophercooper@gmail.com>
2016-06-09 23:34:57 -0700

    Add scope for transferred

--- app/models/procedure_object.rb
+++ app/models/procedure_object.rb
@@ -1,15 +1,18 @@
 class ProcedureObject
   include Mongoid::Document
 
-  belongs_to :data_object
+  belongs_to :data_object, counter_cache: true
 
-  field :type,       type: String
-  field :identifier, type: String
-  field :title,      type: String
-  field :content,    type: String
+  field :type,        type: String
+  field :identifier,  type: String
+  field :title,       type: String
+  field :content,     type: String
+  field :transferred, type: Boolean, default: false
   # fields from remote collectionspace
-  field :csid,       type: String
-  field :uri,        type: String
+  field :csid,        type: String
+  field :uri,         type: String
 
   attr_readonly :type
+
+  scope :transferred, ->{ where(transferred: true) }
 end
