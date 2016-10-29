0d09718ab16c47c5bccf7ea18b44f73b2e3eab6d
Mark Cooper <markchristophercooper@gmail.com>
2016-10-28 18:23:13 -0700

    Begin (broken) collectionobject mapping

--- config/initializers/converters/pastperfect/collectionobject.rb
+++ config/initializers/converters/pastperfect/collectionobject.rb
@@ -6,8 +6,47 @@ module CollectionSpace
       class PastPerfectCollectionObject < CollectionObject
 
         def convert
-          run do |xml|
-            CSXML.add xml, 'objectNumber', attributes["objectid"]
+          run(wrapper: "document") do |xml|
+            xml.send(
+              "ns2:collectionobjects_common",
+              "xmlns:ns2" => "http://collectionspace.org/services/collectionobjects",
+              "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+            ) do
+              # applying namespace breaks import
+              xml.parent.namespace = nil
+              CSXML.add xml, 'objectNumber', attributes["objectid"]
+
+              CSXML.add_group_list xml, 'title', [{
+                "title" => attributes["title"],
+              }]
+
+              CSXML.add xml, 'recordStatus', 'new'
+
+              if attributes.fetch("recfrom", nil)
+                owners = split_mvf attributes, 'recfrom'
+                CSXML::Helpers.add_persons xml, 'owner', owners, :add_repeat
+              end
+            end
+
+            xml.send(
+              "ns2:collectionobjects_bm",
+              "xmlns:ns2" => "http://collectionspace.org/services/collectionobject/local/bm",
+              "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+            ) do
+              # applying namespace breaks import
+              xml.parent.namespace = nil
+              #
+            end
+
+            xml.send(
+              "ns2:collectionobjects_fineart",
+              "xmlns:ns2" => "http://collectionspace.org/services/collectionobject/domain/collectionobject",
+              "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+            ) do
+              # applying namespace breaks import
+              xml.parent.namespace = nil
+              #
+            end
           end
         end
 
