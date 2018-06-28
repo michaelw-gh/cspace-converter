822aa3a939b6231663a0a1c912b9039625ac4e68
Mark Cooper <markchristophercooper@gmail.com>
2018-06-28 16:33:12 -0700

    Fix spec with missing type

--- spec/models/data_object_spec.rb
+++ spec/models/data_object_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe DataObject do
   describe "initialization" do
     let(:data_object_no_type_or_profile) { DataObject.new }
     let(:data_object_bad_type_and_profile) { DataObject.new converter_type: "x", converter_profile: "y" }
-    let(:data_object_ok) { DataObject.new converter_type: "PBM", converter_profile: "acquisition" }
+    let(:data_object_ok) { DataObject.new converter_type: "Vanilla", converter_profile: "acquisition" }
 
     it "requires a converter type" do
       expect(data_object_no_type_or_profile).to be_invalid
