83a202209939daf14961d3e42ab457dba13ad12d
Richard Millet <remillet@yahoo.com>
2018-07-26 07:34:12 -0700

    NOJIRA: Minor formatting changes.

--- config/initializers/utils/cspace.rb
+++ config/initializers/utils/cspace.rb
@@ -221,24 +221,70 @@ module CollectionSpace
       CSXML.add xml, field, get_vocab_urn(field, value)
     end
 
-    def self.get_authority_urn(authority_type, authority, value)
-      CSURN.generate(
-        Rails.application.config.domain,
-        authority_type,
-        authority,
-        CollectionSpace::Identifiers.short_identifier(value),
-        value
-      )
+    #
+    # Get (or create) a URN for an authority term value
+    #
+    def self.get_authority_urn(authority_type, authority_id, value)
+      if value
+        term_parts = get_term_parts value
+
+        display_name = term_parts[:display_name]
+        raise ArgumentError, 'Display name for authority term is missing.' unless display_name != nil
+
+        authority_id = term_parts[:authority_id] != nil ? term_parts[:authority_id] : authority_id
+        raise ArgumentError, 'Authority short ID is missing or empty.' unless authority_id != nil
+
+        term_id = term_parts[:term_id]
+        if term_id == nil
+          term_id = AuthCache::lookup_authority_term_id authority_type, authority_id, display_name
+        end
+
+        #
+        # If the caller didn't supply a short ID and we couldn't find an existing one then
+        # we need to create one.
+        #
+        if term_id == nil
+          term_id = CollectionSpace::Identifiers.short_identifier(display_name)
+        end
+
+        CSURN.generate(
+            Rails.application.config.domain,
+            authority_type,
+            authority_id,
+            term_id,
+            display_name
+        )
+      end
     end
 
-    def self.get_vocab_urn(vocabulary, value, strip = false)
-      CSURN.generate(
-        Rails.application.config.domain,
-        "vocabularies",
-        vocabulary.downcase,
-        CollectionSpace::Identifiers.for_option(value, strip),
-        value
-      )
+    #
+    # Get the URN for a vocabulary term value
+    #
+    def self.get_vocab_urn(vocabulary_id, value, row_number = "unknown", strip = false)
+      if value
+        # try to breakup the term value into component parts
+        term_parts = get_term_parts value
+
+        display_name = term_parts[:display_name]
+        raise ArgumentError, 'Display name for vocabulary term is missing.' unless display_name != nil
+
+        vocabulary_id = term_parts[:authority_id] != nil ? term_parts[:authority_id] : vocabulary_id
+        raise ArgumentError, 'Vocabulary short ID is missing or empty.' unless vocabulary_id != nil
+
+        term_id = term_parts[:term_id]
+        if term_id == nil
+          term_id = AuthCache::lookup_vocabulary_term_id vocabulary_id, display_name
+        end
+        Rails.logger.error "Problem in row #{row_number} because vocabulary short ID for term '#{display_name}' does not exist or was not provided." unless term_id != nil
+
+        CSURN.generate(
+            Rails.application.config.domain,
+            AuthCache::VOCABULARIES,
+            vocabulary_id,
+            term_id,
+            display_name
+        )
+      end
     end
 
   end
