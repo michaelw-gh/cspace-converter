5fd46651e69e5a57d481c19c161a1eda7822c4d5
Richard Millet <remillet@yahoo.com>
2018-08-30 19:42:12 -0700

    NOJIRA: Update to support OHC migration of MH records.

---
+++ config/initializers/converters/ohc/_config.rb
@@ -0,0 +1,57 @@
+module CollectionSpace
+  module Converter
+    module OHC
+
+      def self.registered_procedures
+        [
+          "CollectionObject",
+          "Media",
+        ]
+      end
+
+      def self.registered_profiles
+        {
+          "cataloging" => {
+            "Procedures" => {
+              "CollectionObject" => {
+                "identifier_field" => "objectNumber",
+                "identifier" => "object_number_catalog",
+                "title" => "title",
+              },
+            },
+            "Authorities" => {
+              "Person" => ["content_person", "inscriber", "objectproductionperson", "owners_person"],
+              "Organization" => ["production_org", "owners_org"],
+              "Concept" => [["material", "material_ca"]],
+            },
+            "Relationships" => [
+              {
+              },
+            ],
+          },
+           "media" => {
+             "Procedures" => {
+               "Media" => {
+                 "identifier_field" => "identificationNumber",
+                 "identifier" => "media_id",
+                 "title" => "media_id",
+               },
+             },
+             "Authorities" => {
+             },
+             "Relationships" => [
+               {
+                 "procedure1_type" => "CollectionObject",
+                 "data1_field" => "object_number_catalog",
+                 "procedure2_type" => "Media",
+                 "data2_field" => "media_id",
+               },
+             ],
+           },
+
+        }
+      end
+
+    end
+  end
+end
---
+++ config/initializers/converters/ohc/media.rb
@@ -0,0 +1,27 @@
+module CollectionSpace
+  module Converter
+    module OHC
+      include Default
+
+      class OHCMedia < Media
+
+        def convert
+          run(wrapper: "document") do |xml|
+
+            xml.send(
+                "ns2:media_common",
+                "xmlns:ns2" => "http://collectionspace.org/services/media",
+                "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+            ) do
+              CSXML.add xml, 'identificationNumber', attributes["media_id"]
+              CSXML.add xml, 'title', attributes["title"]
+              CSXML.add xml, 'externalUrl', attributes["externalurl"]
+              CSXML.add xml, 'description', scrub_fields([attributes["description"]])
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
